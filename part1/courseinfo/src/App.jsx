d0a6e94574500a1aa3317354aac70fdedd11fe32
Nicolai Haaber Junge <nicolai.junge@outlook.com>
2023-10-22 13:00:03 +0200

    finished part 1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -21,30 +21,37 @@ const Content = (props) => {
 };
 
 const Total = (props) => {
+  //console.log(props.total);
   return <p>Number of excercises {props.total}</p>;
 };
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const excercise1 = 10;
-  const part2 = "Using props to pass data";
-  const excercise2 = 7;
-  const part3 = "State of a component";
-  const excercise3 = 14;
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
 
   return (
     <div>
       <Header name={course} />
       <Content
-        part1={part1}
-        score1={excercise1}
-        part2={part2}
-        score2={excercise2}
-        part3={part3}
-        score3={excercise3}
+        part1={part1.name}
+        score1={part1.excercises}
+        part2={part2.name}
+        score2={part2.excercises}
+        part3={part3.name}
+        score3={part3.excercises}
       />
-      <Total total={excercise1 + excercise2 + excercise3} />
+      <Total total={part3.exercises + part2.exercises + part1.exercises} />
     </div>
   );
 };
